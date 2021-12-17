fe8964e6fb3761fd348fb129b922d4fd92fa1c4d
Jay <jay0256.cse19@chitkara.edu.in>
2021-12-17 16:35:40 +0530

    Null Bug fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var coinData = [];
-var data =[];
-const render = (coinData)=>{
-    data = coinData.map(coin=> {
+var data = [];
+const render = (coinData) => {
+    data = coinData.map(coin => {
         return (`<div class="coin-container"><div class="coin-row">
             <div class="coin">
             <img src="${coin.image}" alt="imageofcoin"></img>
@@ -10,23 +10,23 @@ const render = (coinData)=>{
             </div>
             <div class="coin_data">
                 <p class="coin-price">₹${coin.current_price}</p>
-                <p class="coin-percent ${coin.price_change_percentage_24h<0?"red":"green"}">${coin.price_change_percentage_24h.toFixed(2)}%</p>
+                <p class="coin-percent ${coin.price_change_percentage_24h < 0 ? "red" : "green"}">${coin.price_change_percentage_24h==null?'NULL':coin.price_change_percentage_24h.toFixed(2)}%</p>
             </div>
             </div></div>`)
-        });
-    document.getElementById("coin-app").innerHTML=data.join("");
-} 
-const handleChange=(e)=>{
+    });
+    document.getElementById("coin-app").innerHTML = data.join("");
+}
+const handleChange = (e) => {
     var x = e.target.value;
-    var filteredcoinData=coinData.filter(coin => coin.name.toLowerCase().includes(x.toLowerCase()));
-    document.getElementById("coin-app").innerHTML="";
+    var filteredcoinData = coinData.filter(coin => coin.name.toLowerCase().includes(x.toLowerCase()));
+    document.getElementById("coin-app").innerHTML = "";
     render(filteredcoinData);
 };
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => res.json())
     .then(res => {
         coinData = res;
-        render(coinData);     
+        render(coinData);
     }).catch(error => {
         console.log(error);
     });
